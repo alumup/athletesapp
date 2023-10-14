417edbd412a947a14853632a44ac0a506b25e08d
Chris Collinsworth <cmcollin41@gmail.com>
2023-10-14 02:04:48 -0600

    fixed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,50 @@
 import "@/styles/globals.css";
-import { cal, inter } from "@/styles/fonts";
+import "@/styles/videojs.css";
+import "@/styles/slick.css";
+import "@/styles/slick-theme.css";
+
 import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
 import { Metadata } from "next";
-import { cn } from "@/lib/utils";
+import { getSiteTheme } from "@/lib/fetchers/server";
+import { headers } from "next/headers";
 
-const title =
-  "Athletes® App";
-const description =
-  "The CRM that helps high school coaches win.";
-const image = "";
+function extractSubdomain(host: string) {
+  const subdomainMatch = host.match(/^(?:www\.)?(.*?)\.(?:[^.]+\.[^.]+)$/);
+
+  if (subdomainMatch) {
+    return subdomainMatch[1];
+  } else {
+    return host;
+  }
+}
+
+const getThemeStyles = async () => {
+  const headerList = headers();
+  const host = headerList.get("host") || "";
+  const siteDomain = extractSubdomain(host);
+  const theme = await getSiteTheme(siteDomain);
+
+  let themeObject = {} as { [key: string]: string };
+
+  for (const category in theme) {
+    for (const variable in theme[category]) {
+      themeObject[`--${variable}`] = theme[category][variable];
+    }
+  }
+
+  return themeObject;
+};
+
+const title = "Athletes® App";
+const description = "A CRM for your athletes and parents.";
+
+const image = "/og-image.png";
 
 export const metadata: Metadata = {
   title,
   description,
-  icons: ["/favicon.ico"],
+  icons: ["https://athletes.app/favicon.ico"],
   openGraph: {
     title,
     description,
@@ -25,21 +55,24 @@ export const metadata: Metadata = {
     title,
     description,
     images: [image],
-    creator: "@cmcollin41",
+    creator: "@athletesapp",
   },
   metadataBase: new URL("https://athletes.app"),
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const themeStyles = await getThemeStyles();
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn(cal.variable, inter.variable)}>
+    <html lang="en" suppressHydrationWarning style={themeStyles}>
+      <head>
+        <link
+          href="https://releases.transloadit.com/uppy/v3.13.1/uppy.min.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
         <Providers>
-          {children}
+          {props.children}
           <Analytics />
         </Providers>
       </body>
