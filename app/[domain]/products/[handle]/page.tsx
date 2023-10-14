beacab8eba43ba1373ab7506110b73af665937f8
Chris Collinsworth <cmcollin41@gmail.com>
2023-10-14 02:00:50 -0600

    fixed product page

--- app/[domain]/products/[handle]/page.tsx
+++ app/[domain]/products/[handle]/page.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from 'next';
-import { notFound } from 'next/navigation';
-import { Suspense } from 'react';
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
-import { GridTileImage } from '@/components/grid/tile';
-import { Gallery } from '@/components/product/gallery';
-import { ProductDescription } from '@/components/product/product-description';
-import { HIDDEN_PRODUCT_TAG } from '@/lib/constants';
-import { getProduct, getProductRecommendations } from '@/lib/shopify';
-import { Image } from '@/lib/shopify/types';
-import Link from 'next/link';
+import { GridTileImage } from "@/components/grid/tile";
+import { Gallery } from "@/components/product/gallery";
+import { ProductDescription } from "@/components/product/product-description";
+import { HIDDEN_PRODUCT_TAG } from "@/lib/constants";
+import { Image } from "@/lib/shopify/types";
+import Link from "next/link";
+import { getAccountShopify } from "@/lib/fetchers/server";
 
 // export const runtime = 'edge';
 
 export async function generateMetadata({
-  params
+  params,
 }: {
-  params: { handle: string };
+  params: { handle: string; domain: string };
 }): Promise<Metadata> {
-  const product = await getProduct(params.handle);
+  const shopify = await getAccountShopify(params.domain);
+
+  const product = await shopify.getProduct(params.handle);
 
   if (!product) return notFound();
 
@@ -32,44 +34,50 @@ export async function generateMetadata({
       follow: indexable,
       googleBot: {
         index: indexable,
-        follow: indexable
-      }
+        follow: indexable,
+      },
     },
     openGraph: url
       ? {
-          images: [
-            {
-              url,
-              width,
-              height,
-              alt
-            }
-          ]
-        }
-      : null
+        images: [
+          {
+            url,
+            width,
+            height,
+            alt,
+          },
+        ],
+      }
+      : null,
   };
 }
 
-export default async function ProductPage({ params }: { params: { handle: string } }) {
-  const product = await getProduct(params.handle);
+export default async function ProductPage({
+  params,
+}: {
+  params: { handle: string; domain: string };
+}) {
+  const shopify = await getAccountShopify(params.domain);
+
+  const product = await shopify.getProduct(params.handle);
 
   if (!product) return notFound();
 
   const productJsonLd = {
-    '@context': 'https://schema.org',
-    '@type': 'Product',
+    "@context": "https://schema.org",
+    "@type": "Product",
     name: product.title,
     description: product.description,
     image: product.featuredImage.url,
     offers: {
-      '@type': 'AggregateOffer',
+      "@type": "AggregateOffer",
       availability: product.availableForSale
-        ? 'https://schema.org/InStock'
-        : 'https://schema.org/OutOfStock',
+        ? "https://schema.org/InStock"
+        : "https://schema.org/OutOfStock",
       priceCurrency: product.priceRange.minVariantPrice.currencyCode,
       highPrice: product.priceRange.maxVariantPrice.amount,
-      lowPrice: product.priceRange.minVariantPrice.amount
-    }
+      lowPrice: product.priceRange.minVariantPrice.amount,
+    },
   };
 
   return (
@@ -77,16 +85,16 @@ export default async function ProductPage({ params }: { params: { handle: string
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify(productJsonLd)
+          __html: JSON.stringify(productJsonLd),
         }}
       />
-      <div className="mx-auto max-w-screen-2xl px-4">
-        <div className="flex flex-col bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
+      <div className="theme mx-auto max-w-screen-2xl px-4">
+        <div className="flex flex-col bg-white p-8 md:p-12 lg:flex-row lg:gap-8">
           <div className="h-full w-full basis-full lg:basis-4/6">
             <Gallery
               images={product.images.map((image: Image) => ({
                 src: image.url,
-                altText: image.altText
+                altText: image.altText,
               }))}
             />
           </div>
@@ -96,15 +104,17 @@ export default async function ProductPage({ params }: { params: { handle: string
           </div>
         </div>
         <Suspense>
-          <RelatedProducts id={product.id} />
+          <RelatedProducts id={product.id} domain={params.domain} />
         </Suspense>
       </div>
     </>
   );
 }
 
-async function RelatedProducts({ id }: { id: string }) {
-  const relatedProducts = await getProductRecommendations(id);
+async function RelatedProducts({ id, domain }: { id: string; domain: string }) {
+  const shopify = await getAccountShopify(domain);
+
+  const relatedProducts = await shopify.getProductRecommendations(id);
 
   if (!relatedProducts.length) return null;
 
@@ -117,13 +127,16 @@ async function RelatedProducts({ id }: { id: string }) {
             key={product.handle}
             className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
           >
-            <Link className="relative h-full w-full" href={`/products/${product.handle}`}>
+            <Link
+              className="relative h-full w-full"
+              href={`/products/${product.handle}`}
+            >
               <GridTileImage
                 alt={product.title}
                 label={{
                   title: product.title,
                   amount: product.priceRange.maxVariantPrice.amount,
-                  currencyCode: product.priceRange.maxVariantPrice.currencyCode
+                  currencyCode: product.priceRange.maxVariantPrice.currencyCode,
                 }}
                 src={product.featuredImage?.url}
                 fill
