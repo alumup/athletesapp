9151ad24d179d7aa2e2dfbed446a7c74bba1bb42
Chris Collinsworth <cmcollin41@gmail.com>
2023-11-12 23:39:37 -0700

    padding

--- components/modal/create-payment-modal.tsx
+++ components/modal/create-payment-modal.tsx
@@ -66,7 +66,7 @@ export default function CreatePaymentModal({account, profile, person, fee, roste
       tabIndex={-1}
       className="relative w-full rounded-md bg-white md:max-w-md md:border md:border-stone-200 md:shadow"
     >
-      <div tabIndex={1} className="relative flex flex-col space-y-4 px-3 pb-20 pt-5 md:p-10">
+      <div tabIndex={1} className="relative flex flex-col space-y-4 px-3 pb-10 pt-5 md:p-10">
         <h2 className="font-cal text-xl md:text-2xl font-bold">Payment</h2>
 
         <div>
