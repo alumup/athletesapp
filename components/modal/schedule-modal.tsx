d181f9a0c723b7370eab5aa467c6ba06de986d5a
Chris Collinsworth <cmcollin41@gmail.com>
2024-09-30 11:02:13 -0600

    resend

--- components/modal/schedule-modal.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import Schedule from "@/app/app/(dashboard)/site/[subdomain]/builder/schedule";
-
-export default function ScheduleModal({ event }: { event: any }) {
-  return <Schedule schedule={event?.schedule} />;
-}
